9826122882baf065b05c82cb378578c50d942de4
rachmawanadi <72264876+rachmawanadi@users.noreply.github.com>
2026-04-27 12:38:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
 import streamlit as st
 import pandas as pd
 
+st.title("Dashboard Jurusan PTN")
+
+# load data
 df = pd.read_csv("dataset_teknik_sipil_indonesia.csv")
 
-st.dataframe(df)
+# search
+search = st.text_input("Cari Jurusan")
+
+# filter kategori
+kategori = st.selectbox(
+    "Filter Kategori",
+    ["Semua"] + sorted(df["kategori"].dropna().unique())
+)
+
+# filter data
+filtered = df.copy()
+
+if search:
+    filtered = filtered[
+        filtered["nama_prodi"].str.contains(search, case=False, na=False)
+    ]
+
+if kategori != "Semua":
+    filtered = filtered[filtered["kategori"] == kategori]
+
+st.write(f"Total data: {len(filtered)}")
+st.dataframe(filtered)
